76094e5d548ecc6345b8d92065aa028b1131d102
nguyen <hoangkhoinguyen3001@gmail.com>
2019-12-07 20:47:15 +0700

    add basic web to cut

---
+++ common/header_common.php
@@ -0,0 +1,25 @@
+<div id="header">
+	<h1><a href="index.html">Belle &amp; Carrie <span>Rehabilitation Yoga</span></a></h1>
+	<ul id="navigation">
+		<li class="current">
+			<a href="../index.php">Home</a>
+		</li>
+		<li>
+			<a href="./page/about.php">About</a>
+		</li>
+		<li>
+			<a href="./classes.php">Classes</a>
+			<ul>
+				<li>
+					<a href="instructors.php">Instructors</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="./contact.php">Contact</a>
+		</li>
+		<li>
+			<a href="./blog.php">Blog</a>
+		</li>
+	</ul>
+</div>
